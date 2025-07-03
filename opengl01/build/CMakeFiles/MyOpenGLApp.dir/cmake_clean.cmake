7a78ab1b6e2b0753732f13cccf6b304429b6a736
Hayden0A <aregrag2005@gmail.com>
2025-07-03 21:13:36 +0100

    dev enviroment

---
+++ opengl01/build/CMakeFiles/MyOpenGLApp.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyOpenGLApp.dir/link.d"
+  "CMakeFiles/MyOpenGLApp.dir/src/glad.c.o"
+  "CMakeFiles/MyOpenGLApp.dir/src/glad.c.o.d"
+  "CMakeFiles/MyOpenGLApp.dir/src/main.cpp.o"
+  "CMakeFiles/MyOpenGLApp.dir/src/main.cpp.o.d"
+  "MyOpenGLApp"
+  "MyOpenGLApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MyOpenGLApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
